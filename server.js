763b53a0e1f08a625a1dc30d15bdb8e0ccd1b0f1
Red Fairy <agorelik2@gmail.com>
2020-09-03 15:28:26 -0400

    main menu function

--- server.js
+++ server.js
@@ -17,4 +17,94 @@ connection.connect((err) => {
 });
 
 // Main Menu
-function mainMenu() {}
+function mainMenu() {
+  return inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What Would you like to do?",
+        name: "mainMenu",
+        choices: [
+          "View All Employees",
+          "View All Departments",
+          "View All Roles",
+          "Add Department",
+          "Add Role",
+          "Add Employee",
+          "Update Employees",
+          "View Employees By Manager",
+          "Update Employee's Manager",
+          "Delete Department",
+          "Delete Role",
+          "Delete Employee",
+          "Quit",
+        ],
+      },
+    ])
+    .then((res) => {
+      switch (res.mainMenu) {
+        case "View All Employees":
+          viewEmployees();
+          break;
+
+        case "View All Departments":
+          viewDepartments();
+          break;
+
+        case "View All Roles":
+          viewRoles();
+          break;
+
+        case "Add Department":
+          addDepartment();
+          break;
+
+        case "Add Role":
+          addRole();
+          break;
+
+        case "Add Employee":
+          addEmployee();
+          break;
+
+        case "Update Employees":
+          updateEmployees();
+          break;
+
+        case "View Employees By Manager":
+          viewEmployeesbyManager();
+          break;
+
+        case "Update Employee's Manager":
+          updateEmployeesManager();
+          break;
+
+        case "Delete Department":
+          deleteDepartment();
+          break;
+
+        case "Delete Role":
+          deleteRole();
+          break;
+
+        case "Delete Employee":
+          deleteEmployee();
+          break;
+        case "Quit":
+        default:
+          connection.end();
+      }
+    });
+}
+function viewEmployees(){};
+function viewDepartments(){};
+function viewRoles(){};
+function addDepartment(){};
+function addRole(){};
+function addEmployee(){};
+function updateEmployees();
+function viewEmployeesbyManager();
+function updateEmployeesManager();
+function deleteDepartment(){};
+function deleteRole(){};
+function deleteEmployee(){};
