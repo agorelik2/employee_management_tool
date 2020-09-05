6ce13deaa2df15a0d5323fb067898e7be228f3cf
Red Fairy <agorelik2@gmail.com>
2020-09-05 17:36:20 -0400

    changed queries for addRole, viewRole

--- server.js
+++ server.js
@@ -133,13 +133,16 @@ function viewDepartments() {
 
 function viewRoles() {
   //"SELECT * FROM roles"
-  connection.query("SELECT * FROM roles;", (err, result) => {
-    if (err) throw err;
+  connection.query(
+    "SELECT roles.id, roles.title, roles.salary, departments.name AS department, departments.id AS department_id FROM roles INNER JOIN departments ON roles.department_id = departments.id",
+    (err, result) => {
+      if (err) throw err;
 
-    console.table(result);
+      console.table(result);
 
-    mainMenu();
-  });
+      mainMenu();
+    }
+  );
 }
 
 function addDepartment() {
@@ -172,58 +175,103 @@ function addDepartment() {
 } //"INSERT INTO departments SET ?",
 
 function addRole() {
-  connection.query("SELECT * FROM roles", function (err, res) {
-    if (err) throw err;
-    console.table(res);
-    // let titleArray = [];
-    // titleArray.push(res[0].title);
-    // console.log(`Response title ${res[0].title}`);
-    inquirer
-      .prompt([
-        {
-          name: "title",
-          type: "input",
-          message: "What is the title of the role you would like to create?",
-        },
-        {
-          name: "salary",
-          type: "input",
-          message: "What is the salary for this role?",
-        },
-        {
-          name: "department_id",
-          type: "input",
-          message: "What is the department id for this role?",
-        },
-      ])
-      .then(function (answer) {
-        console.log(`You have added the role ${answer.title}`);
-        connection.query(
-          "INSERT INTO roles SET ?",
+  connection.query(
+    "SELECT roles.id, roles.title, roles.salary, departments.name AS department, departments.id AS department_id FROM roles INNER JOIN departments ON roles.department_id = departments.id",
+    function (err, res) {
+      if (err) throw err;
+      console.table(res);
+      // let titleArray = [];
+      // titleArray.push(res[0].title);
+      // console.log(`Response title ${res[0].title}`);
+      inquirer
+        .prompt([
           {
-            title: answer.title,
-            salary: answer.salary,
-            department_id: answer.department_id,
+            name: "title",
+            type: "input",
+            message: "What is the title of the role you would like to create?",
           },
-          function (err, res) {
-            if (err) throw err;
-            console.table(res.affectedRows);
-            mainMenu();
-          }
-        );
-      });
-  });
+          {
+            name: "salary",
+            type: "input",
+            message: "What is the salary for this role?",
+          },
+          {
+            name: "department_id",
+            type: "input",
+            message: "What is the department id for this role?",
+          },
+        ])
+        .then(function (answer) {
+          console.log(`You have added the role ${answer.title}`);
+          connection.query(
+            "INSERT INTO roles SET ?",
+            {
+              title: answer.title,
+              salary: answer.salary,
+              department_id: answer.department_id,
+            },
+            function (err, res) {
+              if (err) throw err;
+              console.table(res.affectedRows);
+              mainMenu();
+            }
+          );
+        });
+    }
+  );
 } //"INSERT INTO roles SET ?"
 
 function addEmployee() {
   connection.query(
-    "SELECT employees.id, employees.first_name, employees.last_name, roles.title, departments.name AS department, roles.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employees LEFT JOIN roles on employees.role_id = roles.id LEFT JOIN departments on roles.department_id = departments.id LEFT JOIN employees manager on manager.id = employees.manager_id;",
+    "SELECT employees.id, employees.first_name, employees.last_name, roles.title, roles.id AS roles_id, departments.name AS department,  CONCAT(manager.first_name, ' ', manager.last_name) AS manager, manager.id as manager_id  FROM employees LEFT JOIN roles on employees.role_id = roles.id LEFT JOIN departments on roles.department_id = departments.id LEFT JOIN employees manager on manager.id = employees.manager_id;",
     function (err, res) {
       if (err) throw err;
       console.table(res);
+      inquirer
+        .prompt([
+          {
+            type: "input",
+            message: "Please enter employee's first name",
+            name: "firstName",
+          },
+          {
+            type: "input",
+            message: "Please enter employee last name",
+            name: "lastName",
+          },
+          {
+            type: "input",
+            message: "Enter employee role ID",
+            name: "emplyoeeId",
+          },
+          {
+            type: "input",
+            message: "Please enter manager ID",
+            name: "managerId",
+          },
+        ])
+        .then((answer) => {
+          connection.query(
+            "INSERT INTO employees set ?",
+            {
+              first_name: answer.firstName,
+              last_name: answer.lastName,
+              role_id: answer.emplyoeeId,
+              manager_id: answer.managerId,
+            },
+            (err, result) => {
+              if (err) throw err;
+
+              console.log("Your New employee is added");
+
+              mainMenu();
+            }
+          );
+        });
     }
   );
 } //"INSERT INTO employees SET ?",
+
 function updateEmployees() {} //"UPDATE employees SET role_id = ? WHERE roles_id = ?",
 function viewEmployeesbyManager() {} //"SELECT * FROM employees WHERE manager_id"
 function updateEmployeesManager() {}
